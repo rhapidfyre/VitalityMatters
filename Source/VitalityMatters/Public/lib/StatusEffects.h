95714ad1ff938da6b632d5b85940edabd04e2d83
rhapidfyre <maharris84@hotmail.com>
2023-04-15 16:49:52 -0700

    Added some basic enums and files

---
+++ Source/VitalityMatters/Public/lib/StatusEffects.h
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+// A list of all beneficial effects available
+UENUM(BlueprintType)
+enum class EEffectsBeneficial : uint8
+{
+	NONE		UMETA(DisplayName = "No Effect"),
+	NOURISHED	UMETA(DisplayName = "Nourished"),   // Health Regen
+	HYDRATED	UMETA(DisplayName = "Hydrated"),    // Stamina Regen
+	RESTED		UMETA(DisplayName = "Well Rested"), // Faster
+	FOCUSED		UMETA(DisplayName = "Focused")		// Magic Regen
+};
+
+UENUM(BlueprintType)
+enum class EEffectsDetrimental : uint8
+{
+	NONE	UMETA(DisplayName = "No Effect"),
+	SEASICK	UMETA(DisplayName = "Seasick"),			// Faster Hunger Degradation
+	HEAT	UMETA(DisplayName = "Heat Exhaustion"), // Faster Thirst Degradation
+	COLD	UMETA(DisplayName = "Freezing"),		// Slower Stamina Regen
+	LEG		UMETA(DisplayName = "Leg Injured"),		// Slower move speed
+	DIZZY	UMETA(DisplayName = "Disoriented"),		// Slower magic regen
+	DEAF	UMETA(DisplayName = "Deafened"),		// Muffled Sound Effects
+	ALERT	UMETA(DisplayName = "Vigilant"),		// In Combat or Dangerous Area
+	ARM		UMETA(DisplayName = "Arm Injured"),		// Slower draw/attack times
+	BLIND	UMETA(DisplayName = "Blinded"),			// Vision Obscured
+	POISON	UMETA(DisplayName = "Poisoned"),		// Health Drain
+};
---
+++ Source/VitalityMatters/Public/lib/VitalityEnums.h
@@ -0,0 +1,23 @@
+﻿
+#pragma once
+
+#include "CoreMinimal.h"
+
+UENUM(BlueprintType)
+enum class EVitalityStatus : uint8
+{
+	DEAD		UMETA(Display = "Dead"),
+	ALIVE		UMETA(Display = "Healthy"),
+	INJURED		UMETA(Display = "Injured"),
+	DOWN		UMETA(Display = "Unconscious"),
+};
+
+UENUM(BlueprintType)
+enum class EVitalityCategories : uint8
+{
+	HEALTH		UMETA(DisplayName = "Health"),
+	STAMINA     UMETA(DisplayName = "Stamina"),
+	MAGIC		UMETA(DisplayName = "Magic"),
+	HUNGER		UMETA(DisplayName = "Hunger"),
+	THIRST		UMETA(DisplayName = "Thirst"),
+};
